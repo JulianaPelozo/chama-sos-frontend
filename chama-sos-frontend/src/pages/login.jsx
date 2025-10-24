95c80cd9d2f517534f052422f974e98d58e6b5da
JulianaPelozo <julianapelozopacheco@gmail.com>
2025-10-24 17:07:06 -0300

    Update login

--- chama-sos-frontend/src/pages/login.jsx
+++ chama-sos-frontend/src/pages/login.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Login() {
   return (
     <div className="flex h-screen items-center justify-center bg-gradient-to-br from-red-500 to-red-700">
